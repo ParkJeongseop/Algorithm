3f1f101b943541fdd24b3506c234f98783d0d97e
unknown <parkjeongseop@parkjeongseop.com>
2021-01-30 07:11:06 +0900

    solved 1065

--- Python/1065.py
+++ Python/1065.py
@@ -1,6 +1,18 @@
+def check(n):
+    if n < 100:
+        return True
+    
+    a = int(str(n)[0]) - int(str(n)[1])
+    for i in range(len(str(n))-1):
+        if int(str(n)[i]) - int(str(n)[i+1]) != a:
+            return False
+    return True
+
 n = int(input())
 answer = 0
 
 for i in range(1,n+1):
-    for j in range(i):
-        if(i[j])
+    if check(i):
+        answer += 1
+
+print(answer)
